d96a2e69afb3847d2cfe9bcf29009c3d5ba9e660
Sergey Fasolko <fas.sergey@gmail.com>
2019-05-01 21:19:12 +0300

    create url crawler project

---
+++ MedicinesUrlCrawler/MedicinesUrlCrawler/Medicine.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+
+namespace MedicinesUrlCrawler
+{
+    public class Medicine
+    {
+        public ObjectId Id { get; set; }
+        public string PharmGroup { get; set; }
+        public string Url { get; set; }
+        public string Name { get; set; }
+        public string InternationalName { get; set; }
+        public string ActiveSubstance { get; set; }
+        public string Cooperation { get; set; }
+    }
+}
---
+++ MedicinesUrlCrawler/MedicinesUrlCrawler/UrlRepository.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Driver;
+using System.Configuration;
+using System.Threading.Tasks;
+
+namespace MedicinesUrlCrawler
+{
+    public class MedicineRepository
+    {
+        private readonly IMongoCollection<Medicine> collection;
+        
+        public MedicineRepository(string conn = null)
+        {
+            string connectionString = string.Empty;
+            if (string.IsNullOrEmpty(conn))
+            {
+                connectionString = ConfigurationManager.ConnectionStrings["MongoDB"].ConnectionString;
+            }
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("Medicines");
+            collection = database.GetCollection<Medicine>("MedicinesUrls");
+        }
+
+        public async Task AddMedicine(Medicine medicine)
+        {
+            await collection.InsertOneAsync(medicine);            
+        }
+
+        public async Task<int> GetMedicinesNumber()
+        {
+            return (int)await collection.CountDocumentsAsync(FilterDefinition<Medicine>.Empty);
+        }
+    }
+}
